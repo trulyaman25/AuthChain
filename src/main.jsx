892a70e9c2ecafc12bbab24fbd8184e65e11ea7f
Aman Kumar <trulyaman25@gmail.com>
2024-12-19 23:15:20 +0530

    Initialized Tailwind and Added Home Page

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import RouteConfig from './routes.jsx'
+import './globalStyles.css';
 
 createRoot(document.getElementById('root')).render(
 	<StrictMode>
